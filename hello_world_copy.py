b5e5e7413c13cd6f458bc7251f87c6c133d132b2
rawanvib <43780243+rawanvib@users.noreply.github.com>
2022-02-11 11:42:15 +0530

    removed asyncio object

--- hello_world_copy.py
+++ hello_world_copy.py
@@ -4,8 +4,9 @@ import asyncio
 app = FastAPI()
 
 @app.get("/")
-def hello_world():
+sync def hello_world():
     return {"message": "hello world!!!"}
     
-if __name__ == '__main__':
-    asyncio.run(hello_world)    
+# if __name__ == '__main__':
+#     asyncio.run(hello_world)  
+# asyncio error was there becuase fastapi is supported by python 3.7+
